716a4ca82ad78bca91c2dc56ab88bd13764305c1
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-09-10 13:24:01 -0500

    Updated per exercise 5.3

--- components/Chat.js
+++ components/Chat.js
@@ -13,45 +13,114 @@ import {
 } from 'react-native';
 
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import firebase from 'firebase';
+import firestore from 'firebase';
 
 export default class Chat extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			name: this.props.route.params.userName,
+			user: {
+				_id: '',
+				name: '',
+			},
 			messages: [],
+			uid: 0,
 		};
+
+		const firebaseConfig = {
+			apiKey: 'AIzaSyAz4Erk1QKiEg97-hTUUg7hb1gcGvNz4G4',
+			authDomain: 'chat-app-652fd.firebaseapp.com',
+			projectId: 'chat-app-652fd',
+			storageBucket: 'chat-app-652fd.appspot.com',
+			messagingSenderId: '681539876645',
+			appId: '1:681539876645:web:f4352420b7f7cecad94ccc',
+			measurementId: 'G-DN521Y7J41',
+		};
+
+		if (!firebase.apps.length) {
+			firebase.initializeApp(firebaseConfig);
+		}
+
+		this.referenceChatMessages = firebase.firestore().collection('messages');
 	}
 
 	componentDidMount() {
-		this.setState({
-			messages: [
-				{
-					_id: 1,
-					text: 'Hello developer',
-					createdAt: new Date(),
-					user: {
-						_id: 2,
-						name: 'React Native',
-						avatar: 'https://placeimg.com/140/140/any',
-					},
+		const name = this.props.route.params.userName;
+		// creating a references to messages collection
+		this.referenceMessagesUser = firebase
+			.firestore()
+			.collection('messages')
+			.where('uid', '==', this.state.uid);
+
+		this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+			// if the user is not signed in, they will be signed in anonymously
+			if (!user) {
+				firebase.auth().signInAnonymously();
+			}
+			this.setState({
+				uid: user.uid,
+				messages: [],
+				user: {
+					_id: user.uid,
+					name: name,
 				},
-				{
-					_id: 2,
-					text: this.state.name + ' has entered the chat.',
-					createdAt: new Date(),
-					system: true,
-				},
-			],
+			});
+			this.unsubscribe = this.referenceChatMessages
+				.orderBy('createdAt', 'desc')
+				.onSnapshot(this.onCollectionUpdate);
+		});
+	}
+
+	componentWillUnmount() {
+		this.authUnsubscribe();
+		this.unsubscribe();
+	}
+
+	addMessage() {
+		const newMessage = this.state.messages[0];
+
+		this.referenceChatMessages.add({
+			uid: this.state.uid,
+			_id: newMessage._id,
+			createdAt: newMessage.createdAt,
+			image: newMessage.image || null,
+			text: newMessage.text || '',
+			user: newMessage.user,
 		});
 	}
 
 	onSend(messages = []) {
-		this.setState((previousState) => ({
-			// takes the previous state and adds the new messages to it
-			messages: GiftedChat.append(previousState.messages, messages),
-		}));
+		this.setState(
+			(previousState) => ({
+				// takes the previous state and adds the new messages to it
+				messages: GiftedChat.append(previousState.messages, messages),
+			}),
+			() => {
+				this.addMessage();
+			}
+		);
 	}
+
+	onCollectionUpdate = (querySnapshot) => {
+		const messages = [];
+		// go through each document
+		querySnapshot.forEach((doc) => {
+			// get the QueryDocumentSnapshot's data
+			let data = doc.data();
+			messages.push({
+				_id: data._id,
+				text: data.text,
+				createdAt: data.createdAt.toDate(),
+				user: data.user,
+			});
+		});
+		this.setState({
+			messages,
+		});
+	};
 
 	renderBubble(props) {
 		return (
@@ -103,39 +172,13 @@ export default class Chat extends Component {
 						renderBubble={this.renderBubble.bind(this)}
 						messages={this.state.messages}
 						onSend={(messages) => this.onSend(messages)}
-						user={{
-							_id: 1,
-						}}
+						user={this.state.user}
 					/>
 					{/* The following is included to prevent the keyboard from obsuring the message field */}
 					{Platform.OS === 'android' ? (
 						<KeyboardAvoidingView behavior="height" />
 					) : null}
 				</View>
-
-				{/* Can probably delete this in the near future, but I'm holding on to it for the time being. 
-        <TextInput
-					style={{
-						height: 80,
-						borderColor: 'gray',
-						borderWidth: 2,
-						color: this.props.route.params.textColor,
-					}}
-					onChangeText={(message) => this.setState({ message })}
-					value={this.state.message}
-					placeholder="Type your message..."
-					placeholderTextColor="#596951"
-				/>
-				<TouchableOpacity
-					style={styles.sendMessageButton}
-					title="Start Chatting"
-					onPress={() => {
-						this.alertMyText({ text: this.state.message });
-						// this.alertMyText({ text: this.props.route.params.textColor });
-					}}
-				>
-					<Text style={styles.sendMessageButtonText}>Send</Text>
-				</TouchableOpacity> */}
 			</View>
 		);
 	}
